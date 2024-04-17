e299bc80c7aa27db9d70946659e5daa00b8a1cac
Hieuducemo <115462421+Hieuducemo@users.noreply.github.com>
2024-04-18 01:24:33 +0200

    small change

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import json
 options = Options()
 options.add_experimental_option("detach", True)
 
@@ -11,6 +12,15 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 driver.get("https://apl.unob.cz/StudijniProgramy/Studium/4/Kyberneticka-bezpecnost")
 driver.maximize_window()
 
-programtypes = driver.find_element(By.XPATH,"//span[@class='h2 lead nabidkaH']")
-print(programtypes.text)
+programs = driver.find_element(By.XPATH,"//span[@class='h2 lead nabidkaH']")
+program_text=programs.text
+program_table =driver.find_element(By.XPATH,"//table[@class='table table-sm studijniProgramTable']")
+program_table_text = program_table.text
+data = {"acprograms": program_text,
+        "acprogramtypes":program_table_text}
+
+# Write the data into the JSON file
+with open("systemdata2.json", "w") as json_file:
+    json.dump(data, json_file)
+
 driver.close()
