fd8092201ac6c50919960466b842852037eb6e52
Hieuducemo <115462421+Hieuducemo@users.noreply.github.com>
2024-04-17 22:51:29 +0200

    find_element learning

--- main..py
+++ main..py
@@ -2,10 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options 
 from webdriver_manager.chrome import ChromeDriverManager
-
-options= Options()
+from selenium.webdriver.common.by import By
+options = Options()
 options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 driver.get("https://apl.unob.cz/StudijniProgramy/Studium/4/Kyberneticka-bezpecnost")
+driver.maximize_window()
+
+programtypes = driver.find_element(By.XPATH,"//span[@class='h2 lead nabidkaH']")
+print(programtypes.text)
+driver.close()
