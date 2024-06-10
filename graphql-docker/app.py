1341a0689f39e90e1a88d10415bdace78ae81c50
Hieuducemo <115462421+Hieuducemo@users.noreply.github.com>
2024-06-10 02:01:48 +0200

    new

--- graphql-docker/app.py
+++ graphql-docker/app.py
@@ -77,6 +77,14 @@ from flask_graphql import GraphQLView
 # Mock data lists
 acprograms_data = []
 acsubjects_data = []
+aclessontypes_data = []
+aclessons_data = []
+actopics_data = []
+acclassifications_data = []
+acprogramtypes_data = []
+acsemesters_data = []
+acclassificationtypes_data = []
+acclassificationlevels_data = []
 
 class AcProgram(graphene.ObjectType):
     id = graphene.ID()
@@ -92,15 +100,102 @@ class AcSubject(graphene.ObjectType):
     programId = graphene.String()
     lastchange = graphene.String()
 
+class AcLessonType(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    nameEn = graphene.String()
+    
+    
+class AcLesson(graphene.ObjectType):
+    id = graphene.ID()
+    topicId = graphene.ID()
+    typeId = graphene.ID()
+    lastchange = graphene.String()
+    count = graphene.Int()
+
+class AcTopic(graphene.ObjectType):
+    id = graphene.ID()
+    semesterId = graphene.ID()
+    lastchange = graphene.String()
+    name = graphene.String()
+    nameEn = graphene.String()
+    
+class AcClassification(graphene.ObjectType):
+    id = graphene.ID()
+    userId = graphene.ID()
+    semesterId = graphene.ID()
+    classificationLevelId = graphene.ID()
+    lastchange = graphene.String()
+    order = graphene.Int()
+
+class AcProgramType(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    nameEn = graphene.String()
+    titleId = graphene.ID()
+    formId = graphene.ID()
+    languageId = graphene.ID()
+    levelId = graphene.ID()
+    
+class AcSemester(graphene.ObjectType):
+    id = graphene.ID()
+    subjectId = graphene.ID()
+    classificationtypeId = graphene.ID()
+    lastchange = graphene.String()
+    order = graphene.Int()
+    credits = graphene.Int()
+    
+class AcClassificationType(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    nameEn = graphene.String()
+
+class AcClassificationLevel(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    nameEn = graphene.String()
+
 class Query(graphene.ObjectType):
     acprograms = graphene.List(AcProgram)
     acsubjects = graphene.List(AcSubject)
+    aclessontypes = graphene.List(AcLessonType)
+    aclessons = graphene.List(AcLesson)
+    actopics = graphene.List(AcTopic)
+    acclassifications = graphene.List(AcClassification)
+    acprogramtypes = graphene.List(AcProgramType)
+    acsemesters = graphene.List(AcSemester)
+    acclassificationtypes = graphene.List(AcClassificationType)
+    acclassificationlevels = graphene.List(AcClassificationLevel)
 
     def resolve_acprograms(self, info):
         return acprograms_data
 
     def resolve_acsubjects(self, info):
         return acsubjects_data
+    
+    def resolve_aclessontypes(self, info):
+        return aclessontypes_data
+    
+    def resolve_aclessons(self, info):
+        return aclessons_data
+    
+    def resolve_actopics(self, info):
+        return actopics_data
+    
+    def resolve_acclassifications(self, info):
+        return acclassifications_data
+    
+    def resolve_acprogramtypes(self, info):
+        return acprogramtypes_data
+    
+    def resolve_acsemesters(self, info):
+        return acsemesters_data
+    
+    def resolve_acclassificationtypes(self, info):
+        return acclassificationtypes_data
+    
+    def resolve_acclassificationlevels(self, info):
+        return acclassificationlevels_data
 
 class CreateAcProgram(graphene.Mutation):
     class Arguments:
@@ -131,11 +226,137 @@ class CreateAcSubject(graphene.Mutation):
         acsubject = AcSubject(id=id, name=name, nameEn=nameEn, programId=programId, lastchange=lastchange)
         acsubjects_data.append(acsubject)
         return CreateAcSubject(acsubject=acsubject)
+    
+class CreateAcLessonType(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String()
+        nameEn = graphene.String()
 
+    aclessontype = graphene.Field(lambda: AcLessonType)
+    
+    def mutate(self, info, id, name, nameEn):
+        aclessontype = AcLessonType(id=id, name=name, nameEn=nameEn)
+        aclessontypes_data.append(aclessontype)
+        return CreateAcLessonType(aclessontype=aclessontype)
+    
+class CreateAcLesson(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        topicId = graphene.ID()
+        typeId = graphene.ID()
+        lastchange = graphene.String()
+        count = graphene.Int()
+
+    aclesson = graphene.Field(lambda: AcLesson)
+
+    def mutate(self, info, id, topicId, typeId, lastchange, count):
+        aclesson = AcLesson(id=id, topicId=topicId, typeId=typeId, lastchange=lastchange, count=count)
+        aclessons_data.append(aclesson)
+        return CreateAcLesson(aclesson=aclesson)
+    
+class CreateAcTopic(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        semesterId = graphene.ID()
+        lastchange = graphene.String()
+        name = graphene.String()
+        nameEn = graphene.String()
+
+    actopic = graphene.Field(lambda: AcTopic)
+
+    def mutate(self, info, id, semesterId, lastchange, name, nameEn):
+        actopic = AcTopic(id=id, semesterId=semesterId, lastchange=lastchange, name=name, nameEn=nameEn)
+        actopics_data.append(actopic)
+        return CreateAcTopic(actopic=actopic)
+    
+class CreateAcClassification(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        userId = graphene.ID()
+        semesterId = graphene.ID()
+        classificationLevelId = graphene.ID()
+        lastchange = graphene.String()
+        order = graphene.Int()
+
+    acclassification = graphene.Field(lambda: AcClassification)
+
+    def mutate(self, info, id, userId, semesterId, classificationLevelId, lastchange, order):
+        acclassification = AcClassification(id=id, userId=userId, semesterId=semesterId, classificationLevelId=classificationLevelId, lastchange=lastchange, order=order)
+        acclassifications_data.append(acclassification)
+        return CreateAcClassification(acclassification=acclassification)
+    
+class CreateAcProgramType(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String()
+        nameEn = graphene.String()
+        titleId = graphene.ID()
+        formId = graphene.ID()
+        languageId = graphene.ID()
+        levelId = graphene.ID()
+
+    acprogramtype = graphene.Field(lambda: AcProgramType)
+
+    def mutate(self, info, id, name, nameEn, titleId, formId, languageId, levelId):
+        acprogramtype = AcProgramType(id=id, name=name, nameEn=nameEn, titleId=titleId, formId=formId, languageId=languageId, levelId=levelId)
+        acprogramtypes_data.append(acprogramtype)
+        return CreateAcProgramType(acprogramtype=acprogramtype)
+    
+class CreateAcSemester(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        subjectId = graphene.ID()
+        classificationtypeId = graphene.ID()
+        lastchange = graphene.String()
+        order = graphene.Int()
+        credits = graphene.Int()
+
+    acsemester = graphene.Field(lambda: AcSemester)
+
+    def mutate(self, info, id, subjectId, classificationtypeId, lastchange, order, credits):
+        acsemester = AcSemester(id=id, subjectId=subjectId, classificationtypeId=classificationtypeId, lastchange=lastchange, order=order, credits=credits)
+        acsemesters_data.append(acsemester)
+        return CreateAcSemester(acsemester=acsemester)
+    
+class CreateAcClassificationType(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String()
+        nameEn = graphene.String()
+
+    acclassificationtype = graphene.Field(lambda: AcClassificationType)
+
+    def mutate(self, info, id, name, nameEn):
+        acclassificationtype = AcClassificationType(id=id, name=name, nameEn=nameEn)
+        acclassificationtypes_data.append(acclassificationtype)
+        return CreateAcClassificationType(acclassificationtype=acclassificationtype)
+
+class CreateAcClassificationLevel(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String()
+        nameEn = graphene.String()
+
+    acclassificationlevel = graphene.Field(lambda: AcClassificationLevel)
+
+    def mutate(self, info, id, name, nameEn):
+        acclassificationlevel = AcClassificationLevel(id=id, name=name, nameEn=nameEn)
+        acclassificationlevels_data.append(acclassificationlevel)
+        return CreateAcClassificationLevel(acclassificationlevel=acclassificationlevel)
+    
 class Mutation(graphene.ObjectType):
     create_acprogram = CreateAcProgram.Field()
     create_acsubject = CreateAcSubject.Field()
-
+    create_aclessontype = CreateAcLessonType.Field()
+    create_aclesson = CreateAcLesson.Field()
+    create_actopic = CreateAcTopic.Field()
+    create_acclassification = CreateAcClassification.Field()
+    create_acprogramtype = CreateAcProgramType.Field()
+    create_acsemester = CreateAcSemester.Field()
+    create_acclassificationtype = CreateAcClassificationType.Field()
+    create_acclassificationlevel = CreateAcClassificationLevel.Field()
+    
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
 app = Flask(__name__)
--- graphql-docker/send_data.py
+++ graphql-docker/send_data.py
@@ -195,6 +195,115 @@ mutation($id: ID!, $name: String!, $nameEn: String!, $programId: String!, $lastc
   }
 }
 '''
+mutation_aclessontype = '''
+mutation($id: ID!, $name: String!, $nameEn: String!) {
+  createAclessontype(id: $id, name: $name, nameEn: $nameEn) {
+    aclessontype {
+      id
+      name
+      nameEn
+    }
+  }
+}
+'''
+
+mutation_aclesson = '''
+mutation($id: ID!, $topicId: ID!, $typeId: ID!, $lastchange: String!, $count: Int!) {
+  createAclesson(id: $id, topicId: $topicId, typeId: $typeId, lastchange: $lastchange, count: $count) {
+    aclesson {
+      id
+      topicId
+      typeId
+      lastchange
+      count
+    }
+  }
+}
+'''
+
+mutation_actopic = '''
+mutation($id: ID!, $semesterId: ID!, $lastchange: String!, $name: String!, $nameEn: String!) {
+  createActopic(id: $id, semesterId: $semesterId, lastchange: $lastchange, name: $name, nameEn: $nameEn) {
+    actopic {
+      id
+      semesterId
+      lastchange
+      name
+      nameEn
+    }
+  }
+}
+'''
+
+mutation_acclassification = '''
+mutation($id: ID!, $userId: ID!, $semesterId: ID!, $classificationLevelId: ID!, $lastchange: String!, $order: Int!) {
+  createAcclassification(id: $id, userId: $userId, semesterId: $semesterId, classificationLevelId: $classificationLevelId, lastchange: $lastchange, order: $order) {
+    acclassification {
+      id
+      userId
+      semesterId
+      classificationLevelId
+      lastchange
+      order
+    }
+  }
+}
+'''
+
+mutation_acprogramtype = '''
+mutation($id: ID!, $name: String!, $nameEn: String!, $titleId: ID!, $formId: ID!, $languageId: ID!, $levelId: ID!) {
+  createAcprogramtype(id: $id, name: $name, nameEn: $nameEn, titleId: $titleId, formId: $formId, languageId: $languageId, levelId: $levelId) {
+    acprogramtype {
+      id
+      name
+      nameEn
+      titleId
+      formId
+      languageId
+      levelId
+    }
+  }
+}
+'''
+
+mutation_acsemester = '''
+mutation($id: ID!, $subjectId: ID!, $classificationtypeId: ID!, $lastchange: String!, $order: Int!, $credits: Int!) {
+  createAcsemester(id: $id, subjectId: $subjectId, classificationtypeId: $classificationtypeId, lastchange: $lastchange, order: $order, credits: $credits) {
+    acsemester {
+      id
+      subjectId
+      classificationtypeId
+      lastchange
+      order
+      credits
+    }
+  }
+}
+'''
+
+mutation_acclassificationtype = '''
+mutation($id: ID!, $name: String!, $nameEn: String!) {
+  createAcclassificationtype(id: $id, name: $name, nameEn: $nameEn) {
+    acclassificationtype {
+      id
+      name
+      nameEn
+    }
+  }
+}
+'''
+
+mutation_acclassificationlevel = '''
+mutation($id: ID!, $name: String!, $nameEn: String!) {
+  createAcclassificationlevel(id: $id, name: $name, nameEn: $nameEn) {
+    acclassificationlevel {
+      id
+      name
+      nameEn
+    }
+  }
+}
+'''
 
 # Function to send mutation request
 def send_mutation(url, mutation, variables):
@@ -227,3 +336,93 @@ for subject in data['acsubjects']:
     }
     result = send_mutation(url, mutation_acsubject, variables)
     print('AcSubject imported successfully:', result)
+
+# Send aclessontypes data
+for lessontype in data['aclessontypes']:
+    variables = {
+        'id': lessontype['id'],
+        'name': lessontype['name'],
+        'nameEn': lessontype['name_en']
+    }
+    result = send_mutation(url, mutation_aclessontype, variables)
+    print('AcLessonType imported successfully:', result)
+    
+# Send aclessons data
+for lesson in data['aclesson']:
+    variables = {
+        'id': lesson['id'],
+        'topicId': lesson['topic_id'],
+        'typeId': lesson['type_id'],
+        'lastchange': lesson['lastchange'],
+        'count': lesson['count']
+    }
+    result = send_mutation(url, mutation_aclesson, variables)
+    print('AcLesson imported successfully:', result)
+
+# Send actopic data
+for topic in data['actopic']:
+    variables = {
+        'id': topic['id'],
+        'semesterId': topic['semester_id'],
+        'lastchange': topic['lastchange'],
+        'name': topic['name'],
+        'nameEn': topic['name_en']
+    }
+    result = send_mutation(url, mutation_actopic, variables)
+    print('AcTopic imported successfully:', result)
+
+# Send acclassifications data
+for classification in data['acclassification']:
+    variables = {
+        'id': classification['id'],
+        'userId': classification['user_id'],
+        'semesterId': classification['semester_id'],
+        'classificationLevelId': classification['classificationlevel_id'],
+        'lastchange': classification['lastchange'],
+        'order': classification['order']
+    }
+    result = send_mutation(url, mutation_acclassification, variables)
+    print('AcClassification imported successfully:', result)
+    
+for programtype in data['acprogramtypes']:
+    variables = {
+        'id': programtype['id'],
+        'name': programtype['name'],
+        'nameEn': programtype['name_en'],
+        'titleId': programtype['title_id'],
+        'formId': programtype['form_id'],
+        'languageId': programtype['language_id'],
+        'levelId': programtype['level_id']
+    }
+    result = send_mutation(url, mutation_acprogramtype, variables)
+    print('AcProgramtypes imported successfully:', result)
+
+for semester in data['acsemester']:
+    variables = {
+        'id': semester['id'],
+        'subjectId': semester['subject_id'],
+        'classificationtypeId': semester['classificationtype_id'],
+        'lastchange': semester['lastchange'],
+        'order': semester['order'],
+        'credits': semester['credits']
+    }
+    result = send_mutation(url, mutation_acsemester, variables)
+    print('AcSemesters imported successfully:', result)
+    
+for classificationtype in data['acclassificationtypes']:
+    variables = {
+        'id': classificationtype['id'],
+        'name': classificationtype['name'],
+        'nameEn': classificationtype['name_en'],
+    }
+    result = send_mutation(url, mutation_acclassificationtype, variables)
+    print('AcClassificationtypes imported successfully:', result)
+    
+for classificationlevel in data['acclassificationlevels']:
+    variables = {
+        'id': classificationlevel['id'],
+        'name': classificationlevel['name'],
+        'nameEn': classificationlevel['name_en'],
+    }
+    result = send_mutation(url, mutation_acclassificationlevel, variables)
+    print('AcClassificationlevels imported successfully:', result)
